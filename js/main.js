2f9e88f8420c785b591d6f0d235c05fbb4244f20
aumartinez <acm_asd@hotmail.com>
2019-08-22 20:10:40 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 //JS script
 
 function run() {
-  let elem = ;
+  let elems = document.querySelectorAll("*");
+  let spyelems = [];
+    
+  for(let i = 0; i < elems.length; i++){    
+    if (elems[i].dataset.spy == "scroll") {
+      spyelems.push(elems[i]);      
+    }
+  }
 }
 
 window.onload = run;
