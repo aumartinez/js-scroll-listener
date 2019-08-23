559a727f6e5c6eec0119dcc28628a3fe65e5e9b2
aumartinez <acm_asd@hotmail.com>
2019-08-22 20:15:45 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,27 @@
 function run() {
   let elems = document.querySelectorAll("*");
   let spyelems = [];
-    
-  for(let i = 0; i < elems.length; i++){    
+  
+  //Get all spy elements  
+  for (let i = 0; i < elems.length; i++) {
     if (elems[i].dataset.spy == "scroll") {
       spyelems.push(elems[i]);      
     }
   }
+  
+  elemsArr(spyelems);  
+}
+
+function elemsArr(arr) {
+  let elempos;
+  
+  for (let i = 0; i < arr.length; i++) {
+    getElemsPos(arr[i]);
+  }
+}
+
+function getElemsPos(elem) {
+  
 }
 
 window.onload = run;
